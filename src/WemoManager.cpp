608b3a88f32446942f2c14530d5abb479f6c263f
igorantolic <iantolic@gmail.com>
2017-12-11 21:55:52 +0100

    Update WemoManager.cpp

--- src/WemoManager.cpp
+++ src/WemoManager.cpp
@@ -67,10 +67,10 @@ void WemoManager::serverLoop(){
 
     // check if this is a M-SEARCH for WeMo device
     String request = String((char *)packetBuffer);
-    // Serial.println("----------");
-    // Serial.println(request);
-    // Serial.println("-----------");
-    if(request.indexOf('M-SEARCH') > 0) {
+     //Serial.println("----------");
+     //Serial.println(request);
+     //Serial.println("-----------");
+    if(request.indexOf("M-SEARCH") >= 0) {
         if((request.indexOf("urn:Belkin:device:**") > 0) || (request.indexOf("ssdp:all") > 0) || (request.indexOf("upnp:rootdevice") > 0)) {
           Serial.println("Got UDP Belkin Request..");
 
